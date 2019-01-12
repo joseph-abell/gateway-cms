aa84314a30dc5930b98e383a77a7cd9240d920bf
Joseph Abell <joseph.abell@piksel.com>
2019-01-12 20:41:44 +0000

    make draft non required

--- update-failed.js
+++ update-failed.js
@@ -2,5 +2,4 @@ const failed = require('./failed.json');
 const fs = require('fs');
 
 const result = failed.urls.map(f => ({ old: f, n: '' }))
-console.log(fledailed);
 fs.writeFileSync('./failed.json', JSON.stringify(result, null, 2), 'utf8');
