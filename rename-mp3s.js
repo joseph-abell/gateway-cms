01afca82a851bbff1bdfa395b4e1a7857117f43c
Joseph Abell <joseph.abell@piksel.com>
2019-01-12 16:48:03 +0000

    encode urls

--- rename-mp3s.js
+++ rename-mp3s.js
@@ -1,16 +1,19 @@
 const glob = require('glob');
 const fs = require('fs');
 const flat = require('flat');
+const encodeurl = require('encodeurl');
 
 glob('data/words/*.*', {}, (err, filenames) => {
   filenames.forEach(filename => {
     const file = JSON.parse(fs.readFileSync(filename, 'utf8'));
     const flatFile = flat(file);
-    const mp3s = Object.values(flatFile).filter(f => typeof f === 'string' && f.includes('.mp3') && f.startsWith('/uploads')).map(f => ({old: f, n: `https://s3-eu-west-1.amazonaws.com/gatewaychurchyork${f}`}));
+    const mp3s = Object.values(flatFile).filter(
+      f => typeof f === 'string' && f.includes('.mp3'),
+    ).map(old => ({old, n: encodeurl(old)}));
     let result = JSON.stringify(file, null, 2);
     mp3s.forEach(mp3 => {
-      result = result.split(mp3.old).join(mp3.n);
+      result = result.split(mp3.old).join(mp3.n)
     });
     fs.writeFileSync(filename, result, 'utf8');
-  })
+  });
 });
