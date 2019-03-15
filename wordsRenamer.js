18447579e283b7203b35191bdd41b6550b6803ef
Joseph Abell <joseph.abell@piksel.com>
2019-03-15 11:29:11 +0000

    more changes to the renamer

--- wordsRenamer.js
+++ wordsRenamer.js
@@ -2,23 +2,26 @@ const glob = require('glob');
 const fs = require('fs');
 
 glob('./data/words/*', null, (err, files) => {
-  files.forEach(file => {
-    const data = JSON.parse(fs.readFileSync(file, 'utf-8'));
-    const splitAudioFile = data.audioFile && data.audioFile.split('/');
-    let audioNumber =
-      splitAudioFile &&
-      splitAudioFile
-        .pop()
-        .split('%2520')[0]
-        .split('_')[0]
-        .split('-')[0];
-    if (audioNumber && audioNumber.length < 8) {
-      audioNumber = `20${audioNumber}`;
-    }
-    const audioDirectory = splitAudioFile && splitAudioFile.join('/');
-    const audioFileName = audioNumber && `${audioDirectory}/${audioNumber}.mp3`;
+  files
+    .filter(file => file !== './data/words/index.json')
+    .forEach(file => {
+      const data = JSON.parse(fs.readFileSync(file, 'utf-8'));
+      const splitAudioFile = data.audioFile && data.audioFile.split('/');
+      let audioNumber =
+        splitAudioFile &&
+        splitAudioFile
+          .pop()
+          .split('%2520')[0]
+          .split('_')[0]
+          .split('-')[0];
+      if (audioNumber && audioNumber.length < 8) {
+        audioNumber = `20${audioNumber}`;
+      }
+      const audioDirectory = splitAudioFile && splitAudioFile.join('/');
+      const audioFileName =
+        audioNumber && `${audioDirectory}/${audioNumber}.mp3`;
 
-    data.audioFile = audioFileName;
-    fs.writeFileSync(file, data, 'utf-8');
-  });
+      data.audioFile = audioFileName;
+      fs.writeFileSync(file, JSON.stringify(data, null, 2), 'utf-8');
+    });
 });
