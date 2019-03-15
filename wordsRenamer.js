ce385a497457ba4131b1c1c6ea3a4ce683d9e7a6
Joseph Abell <joseph.abell@piksel.com>
2019-03-15 11:26:13 +0000

    add words renamer

---
+++ wordsRenamer.js
@@ -0,0 +1,24 @@
+const glob = require('glob');
+const fs = require('fs');
+
+glob('./data/words/*', null, (err, files) => {
+  files.forEach(file => {
+    const data = JSON.parse(fs.readFileSync(file, 'utf-8'));
+    const splitAudioFile = data.audioFile && data.audioFile.split('/');
+    let audioNumber =
+      splitAudioFile &&
+      splitAudioFile
+        .pop()
+        .split('%2520')[0]
+        .split('_')[0]
+        .split('-')[0];
+    if (audioNumber && audioNumber.length < 8) {
+      audioNumber = `20${audioNumber}`;
+    }
+    const audioDirectory = splitAudioFile && splitAudioFile.join('/');
+    const audioFileName = audioNumber && `${audioDirectory}/${audioNumber}.mp3`;
+
+    data.audioFile = audioFileName;
+    fs.writeFileSync(file, data, 'utf-8');
+  });
+});
