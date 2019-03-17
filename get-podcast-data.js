6ba85b826ff5833a5fed4949fa6866c5ae9179a9
Joseph Abell <joseph.abell@piksel.com>
2019-03-17 21:12:48 +0000

    podcast-data

--- get-podcast-data.js
+++ get-podcast-data.js
@@ -23,23 +23,43 @@ function httpGet(url) {
     'http://gateway-cms.netlify.com/data/words/index.json',
   );
   const wordsData = await wordsRequest.json();
-  const podcasts = Object.entries(wordsData)
-    .filter(word => word[1].data.showOnPodcast)
+  const podcasts = Object.entries(wordsData).filter(
+    word => word[1].data.showOnPodcast,
+  );
 
-    // Setting podcastFile section
-    .forEach(word => {
-      const wordData = word[1];
-      const localDirectory = `./data/words/${word[0]}`;
+  // Setting podcastFile section
+  podcasts.forEach(word => {
+    const wordData = word[1];
+    const localDirectory = `./data/words/${word[0]}`;
 
-      if (!wordData.podcastFile) {
-        const fileData = JSON.parse(fs.readFileSync(localDirectory, 'utf-8'));
-        fileData.podcastFile = fileData.audioFile || fileData.file;
-        fs.writeFileSync(
-          localDirectory,
-          JSON.stringify(fileData, null, 2),
-          'utf-8',
-        );
+    if (
+      !wordData.podcastFile ||
+      wordData.podcastFile !== (wordData.audioFile || wordData.file)
+    ) {
+      const fileData = JSON.parse(fs.readFileSync(localDirectory, 'utf-8'));
+
+      fileData.podcastFile = fileData.audioFile || fileData.file;
+
+      fs.writeFileSync(
+        localDirectory,
+        JSON.stringify(fileData, null, 2),
+        'utf-8',
+      );
+    }
+  });
+
+  let podcastCount = 0;
+  podcasts.forEach(async podcast => {
+    const item = podcast[1];
+    if (podcastCount > 4) return;
+    if (!item.podcastMetadata) {
+      podcastCount = podcastCount + 1;
+      const podcastFile = item.data.podcastFile || '';
+      console.log(podcastFile);
+      if (podcastFile.length) {
+        const httpData = await httpGet(audioFile);
+        console.log(httpData);
       }
-    });
-  // console.log(podcasts);
+    }
+  });
 })();
